a3d8cdef061f0a5f021f226aafc41ccb41fc5626
Sebastian Rendon Arteaga <serendona@unal.edu.co>
2023-02-04 23:51:00 -0500

    Initial Sacaffold

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './pages/auth/auth.component';
+import { HomeComponent } from './pages/home/home.component';
+import { InitializerService } from './services/initializer/initializer.service';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: AuthComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeApp,
+      deps: [InitializerService],
+      multi: true,
+    },
+  ],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+
+export function initializeApp(initializer: InitializerService) {
+  return (): Promise<void> => {
+    return initializer.init();
+  };
+}
---
+++ src/app/services/initializer/initializer.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InitializerService {
+  constructor() {}
+
+  init() {
+    return new Promise<void>((resolve) => {
+      resolve();
+    });
+  }
+}
